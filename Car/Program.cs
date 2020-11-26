283a201ce26ffbf157ecec98659d2ee6cace62c7
Opilane TTHK <opilane@tthk.ee>
2020-11-26 13:21:26 +0200

    challenge

---
+++ Car/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Car
+{
+    class Program
+    {
+
+        class Car
+        {
+            string markName;
+            string modelName;
+            int registrationNumber;
+            string colour;
+            int odometer;
+            int fuelTank;
+
+            public Car(string _markName, string _modelName, string _colour)
+            {
+                markName = _markName;
+                modelName = _modelName;
+                colour = _colour;
+                odometer = 0;
+                fuelTank = 60;
+            }
+
+            public int Odometer
+            {
+                get { return odometer; }
+            }
+
+            public void Drive()
+            {
+                Console.WriteLine("The car has driven a lap");
+                fuelTank =- 5;
+                odometer = +100;
+            }
+
+            public void ShowCarData()
+            {
+                Console.WriteLine($"The cars name is {markName}, mark number is {}");
+            }
+
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            Car newCar = new Car("", )
+            Console.WriteLine("");
+        }
+    }
+}
